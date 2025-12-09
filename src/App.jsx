be66f8a98c55a2b87fbd3d7c5f26ca5b9625d2fb
avneet <avneet@smartgeeks.in>
2025-12-09 11:21:08 +0530

    Initialized Vite+ React Project and Configured Tailwind (PostCSS + Tailwind Config)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-4xl text-blue-600 p-4">
+      This is a test!
+    </h1>
+  );
+}
